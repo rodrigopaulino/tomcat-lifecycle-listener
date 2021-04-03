3c0b80e5c43988c6dea687826e5c3236260e36ee
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2021-04-03 18:37:17 -0300

    Implementing a Tomcat's LifecycleListener

--- src/main/java/rodrigopaulino/tomcatlifecyclelisteners/StartupNotificationLifecycleListener.java
+++ src/main/java/rodrigopaulino/tomcatlifecyclelisteners/StartupNotificationLifecycleListener.java
@@ -1,5 +1,18 @@
 package rodrigopaulino.tomcatlifecyclelisteners;
 
-public class StartupNotificationLifecycleListener {
-  
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+/**
+ * @author Rodrigo Paulino
+ */
+public class StartupNotificationLifecycleListener implements LifecycleListener {
+
+    @Override
+    public void lifecycleEvent(LifecycleEvent event) {
+        if (Lifecycle.AFTER_START_EVENT.equals(event.getType())) {
+            System.out.println("Rodrigo's Lifecycle Event");
+        }
+    }
 }
