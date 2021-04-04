3d9690f51cec35243211b60ed2eb9bde9fc87fbf
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2021-04-04 12:40:33 -0300

    Use Liferay's formatter

--- src/main/java/rodrigopaulino/tomcatlifecyclelisteners/StartupNotificationLifecycleListener.java
+++ src/main/java/rodrigopaulino/tomcatlifecyclelisteners/StartupNotificationLifecycleListener.java
@@ -1,36 +1,43 @@
 package rodrigopaulino.tomcatlifecyclelisteners;
 
+import java.io.IOException;
+
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleEvent;
 import org.apache.catalina.LifecycleListener;
-
-import java.io.IOException;
 
 /**
  * @author Rodrigo Paulino
  */
 public class StartupNotificationLifecycleListener implements LifecycleListener {
 
-    @Override
-    public void lifecycleEvent(LifecycleEvent event) {
-        if (Lifecycle.AFTER_START_EVENT.equals(event.getType())) {
-            String osName = System.getProperty("os.name").toLowerCase();
+	@Override
+	public void lifecycleEvent(LifecycleEvent event) {
+		if (Lifecycle.AFTER_START_EVENT.equals(event.getType())) {
+			String osName = System.getProperty("os.name");
 
-            if (osName.startsWith("mac")) {
-                ProcessBuilder builder = new ProcessBuilder(
-                        "osascript", "-e",
-                        "display notification \"Your app is running\""
-                                + " with title \"Tomcat\"");
-                try {
-                    builder.inheritIO().start();
-                } catch (IOException e) {
-                   System.out.println(e);
-                }
-            }
-            else if (osName.startsWith("windows")) {
-            }
-            else {
-            }
-        }
-    }
+			osName = osName.toLowerCase();
+
+			if (osName.startsWith("mac")) {
+				ProcessBuilder builder = new ProcessBuilder(
+					"osascript", "-e",
+					"display notification \"Your app is running\" with title " +
+						"\"Tomcat\"");
+
+				try {
+					ProcessBuilder processBuilder = builder.inheritIO();
+
+					processBuilder.start();
+				}
+				catch (IOException e) {
+					System.out.println(e);
+				}
+			}
+			else if (osName.startsWith("windows")) {
+			}
+			else {
+			}
+		}
+	}
+
 }
